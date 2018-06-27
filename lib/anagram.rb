46641f775432153875cb2ece762869ea5f6e8748
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 13:50:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :anagram
+
+    def initialize(anagram)
+        @anagram = anagram
+    end
+
+    def match(list)
+        list.select {|word| word.split(//).sort == @anagram.to_s.split(//).sort}
+    end
+end
