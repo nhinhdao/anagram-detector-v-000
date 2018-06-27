a0253c7c0c39fce43289c0bd5c4a6ed2c29b4405
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-27 13:50:19 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,9 +4,9 @@ class Anagram
 
     # ANAGRAM = ""
 
-    def initialize(anagram)
-        @anagram = anagram
-    end
+    # def initialize(anagram)
+    #     @anagram = anagram
+    # end
 
     def match(list)
         list.select {|word| word.split(//).sort == @anagram.to_s.split(//).sort}
